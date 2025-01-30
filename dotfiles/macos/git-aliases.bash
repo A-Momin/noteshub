9486c2d0dd778b8e1ce3b89692dbd1d943c29190
Aminul Momin <bbcredcap3@gmail.com>
2025-01-29 22:26:16 -0600

    updates

--- dotfiles/macos/git-aliases.bash
+++ dotfiles/macos/git-aliases.bash
@@ -9,7 +9,7 @@ alias gc="git checkout $1"
 # Git
 alias gc="git commit -m"
 alias gca="git commit -a -m"
-alias gp="git push origin HEAD"
+alias gp="git push origin $1"
 alias gpu="git pull origin"
 alias gst="git status"
 alias gl="git log --graph --topo-order --pretty='%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N' --abbrev-commit"
